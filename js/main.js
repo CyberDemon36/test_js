f2ea912b6974fec682295e46c0c62606e7cd6c0d
Ruslan <anonim111@.ua>
2020-10-31 16:00:53 +0200

    Creating new repository

---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict"; // позволяет установить современный формат кода
+
+// const result = confirm("Are you still here?");
+// console.log(result);
+
+// const answer = +prompt("Тебе есть 18?", "18");
+// console.log(answer + 5);
+
+// document.write - заменяет все содержимое сайта 
+// на результат вывода этой команды
+
+// массив - частный случай объекта 
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/3`);
+
+const user = 'Ruslan';
+alert(`Hello, ${user}`);
